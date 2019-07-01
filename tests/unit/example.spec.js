4bd29a093a85b08289b914c1a6140c90b112c5c8
sarwanputeh <bazzarm.sarwan@gmail.com>
2019-07-01 10:18:05 +0700

    can use my component

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue'
 
-describe('HelloWorld.vue', () => {
+describe('Counter.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const msg = 'Increment'
+    const wrapper = shallowMount(Counter, {
+      propsData: {
+        msg: msg
+      }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch('Increment')
   })
 })
+// import { shallowMount } from '@vue/test-utils'
+// import slides from '@/components/slides.vue'
+// const bazComponent = {
+//   name: 'baz-component',
+//   template: '<p>baz</p>'
+// }
+
+// const wrapper = shallowMount(Component, {
+//   slots: {
+//     default: [slides, '<my-component />', 'text'],
+//     media: slides, // Will match `<slot name="FooBar" />`.
+//     qux: '<my-component />'
+//   }
+// })
+
+// expect(wrapper.find('div')).toBe(true)
